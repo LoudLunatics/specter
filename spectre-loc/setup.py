9d4977061cb8a0a45f5bff1e42dbb902d80c2679
LoudLunatics <nauvalazfa@proton.me>
2026-04-17 16:05:01 +0700

    Update setup.py

--- spectre-loc/setup.py
+++ spectre-loc/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='spectre-loc',
-    version='1.0.1', # Kita naikkan versinya karena ini perbaikan
-    packages=find_packages(), # 🚨 INI SANGAT WAJIB ADA
+    version='1.0.2', # Kita naikkan ke 1.0.2 agar Pacman tahu ada update
+    packages=['spectre'], # 🚨 CARA EKSPLISIT: Paksa Python membungkus folder 'spectre'
     install_requires=[
         'shodan==1.31.0',
         'python-dotenv==1.0.1',
