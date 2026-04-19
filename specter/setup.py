b69e30c2e768bb4749ab8626701eef04e88fdc13
LoudLunatics <nauvalazfa@proton.me>
2026-04-19 09:00:48 +0700

    Update setup.py

--- specter/setup.py
+++ specter/setup.py
@@ -3,15 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='specter',
     version='1.2.0',
-    # find_packages() akan otomatis mencari folder yang ada init.py nya
+    description='Ghost Network Recon Engine (Standalone Nmap Scanner)',
+    author='LoudLunatics',
+    # find_packages() otomatis mencari folder yang punya init.py
     packages=find_packages(),
+    # Menginstruksikan Python untuk menyertakan file non-python jika ada
     include_package_data=True,
+    python_requires='>=3.6',
     install_requires=[
         'python-dotenv',
         'rich',
+        # nmap tidak perlu ditulis di sini karena diurus oleh pacman (depends)
     ],
     entry_points={
         'console_scripts': [
+            # perintah_di_terminal = nama_folder.nama_file:nama_fungsi
             'specter=specter.cli:main',
         ],
     },
