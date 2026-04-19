ab40f167521f0440f21227e635f0c798f5c0d0b5
LoudLunatics <nauvalazfa@proton.me>
2026-04-19 08:50:01 +0700

    Update setup.py

--- specter/setup.py
+++ specter/setup.py
@@ -1,24 +1,18 @@
-# Maintainer: LoudLunatics <nauvalazfa@proton.me>
-pkgname=specter
-pkgver=1.2.0
-pkgrel=1
-pkgdesc="Ghost Network Recon Engine"
-arch=('any')
-url="https://github.com/LoudLunatics/specter"
-license=('MIT')
-depends=('python' 'python-dotenv' 'python-rich' 'nmap')
-makedepends=('python-build' 'python-installer' 'python-wheel' 'python-setuptools')
+from setuptools import setup, find_packages
 
-source=("$pkgname-main.tar.gz::https://github.com/LoudLunatics/$pkgname/archive/refs/heads/main.tar.gz")
-sha256sums=('SKIP')
-
-build() {
-  cd "$pkgname-main"
-  # Pastikan kita membangun dari folder yang ada setup.py nya
-  python -m build --wheel --no-isolation
-}
-
-package() {
-  cd "$pkgname-main"
-  python -m installer --destdir="$pkgdir" dist/*.whl
-}
+setup(
+    name='specter',
+    version='1.2.0',
+    # find_packages() akan otomatis mencari folder yang ada init.py nya
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dotenv',
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'specter=specter.cli:main',
+        ],
+    },
+)
