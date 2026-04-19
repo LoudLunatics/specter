77c4c13d49042fa040dd8e07c8334cf42e144510
LoudLunatics <nauvalazfa@proton.me>
2026-04-19 07:31:18 +0700

    Update setup.py

--- spectre-loc/setup.py
+++ spectre-loc/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='specter',          # Berubah jadi specter
-    version='1.2.2',         # Versi baru
+    version='1.2.0',         # Versi baru
     packages=['specter'],    # Target folder baru
     install_requires=[
         'python-dotenv==1.0.1',
