be47b53b0f22838e9ec4a61f6d3465b15094f401
LoudLunatics <nauvalazfa@proton.me>
2026-04-19 08:27:09 +0700

    move setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='specter',          # Berubah jadi specter
+    version='1.2.0',         # Versi baru
+    packages=['specter'],    # Target folder baru
+    install_requires=[
+        'python-dotenv==1.0.1',
+        'rich==13.7.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'specter=specter.cli:main',  # Perintah panggilannya sekarang 'specter'
+        ],
+    },
+)
